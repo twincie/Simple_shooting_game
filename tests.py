a315f274a2ce50ec1943a0b88faa3bf75b6f4add
Twincie <oyebodej2@gmail.com>
2022-06-22 17:28:11 +0100

    removed unwanted errors and corrected main

--- tests.py
+++ tests.py
@@ -1,24 +1,27 @@
 import random
+import sys
 
 class player:
     def __init__(self, name: str, life=5):
         self.name = name
         self.life = life
-        self.bullet = 5
+        self.bullet = 30
     
     def is_shot(self):
         choice = random.randint(1,2)
         if choice == 1:
             return True
+        return False
+
+    def hit(self, other):
+        other.life -= 1
+        if other.life <= 0:
+            print(other.name + ' was killed')
+            print('game over ')
+            print(self.name +' won')
+            sys.exit()
         else:
-            return False
-
-    def life_remaining(self):
-        return f'{self.name} life remains {self.life}'
-
-    def bullet_remaining(self):
-        self.bullet -= 1
-        return f'{self.name} bullet remains {self.bullet}'
+            return f'{other.name} has been shot'
 
     def take_cover(self, other):
         covers = ['behind a log','behind a tree','behind a stump','in a ravine','in the hollow','in the trenches','behind a wall','in a rubble','in a crater']
@@ -27,30 +30,29 @@ class player:
 
     def shoot(self, other):
         print(self.name, 'takes a shot')
-        if self.is_shot is True:
-            other.life -= 1
-            if other.life > 0:
-                return (other.life_remaining())
-            else:
-                return f'{other.life_remaining()} \n{self.bullet_remaining()} \n{other.name} is dead.'
+        self.bullet -= 1
+        choice = self.is_shot()
+        if choice is True:
+            return self.hit(other)
         else:
-            return f'{other.life_remaining()} \n{self.bullet_remaining()} \n{self.take_cover(other)}'
-            
+            covers = ['behind a log','behind a tree','behind a stump','in a ravine','in the hollow','in the trenches','behind a wall','in a rubble','in a crater']
+            choices = random.choice(covers)
+            return f'{other.name} took cover {choices}'
 
 def main():
     First_player = input('First player name: ')
     Second_player = input('Second player name: ')
+    player1 = player(First_player)
+    player2 = player(Second_player)
     round = 0
     while True:
         round += 1
-        print ("round",round)
-        player1 = player(First_player)
-        player2 = player(Second_player)
-        print('player1:',player1.__dict__)
-        print('player2:',player2.__dict__)
+        print ('round',round)
         print(player1.shoot(player2))
         print(player2.shoot(player1))
-        input('go to the next round\n\n')
+        print(f'LIVES Remaining:[{player1.name}:{player1.life},{player2.name}:{player2.life}]')
+        print(f'BULLET Remaining:[{player1.name}:{player1.life},{player2.name}:{player2.life}]')
+        print('\n')
 
 if __name__ =='__main__':
     main()
